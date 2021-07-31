57e5d089c1d8acf5d25edbd70e36d41fb7477c14
Luiz Henrique <luizhjramos@outlook.com>
2021-07-31 12:38:25 -0300

    Aula 2 - Tipo any

---
+++ src/A002-tipo-any/A002-tipo-any.ts
@@ -0,0 +1,11 @@
+// Utilize any só em ultimos casos
+
+function showMessage(msg: any) {
+  return msg
+}
+
+console.log(showMessage([1, 2, 3]))
+console.log(showMessage('hello world'))
+console.log(showMessage(2))
+
+//aceita qualquer coisa
