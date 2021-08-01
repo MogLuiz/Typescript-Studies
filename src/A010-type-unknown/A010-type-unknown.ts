7dbdfd87916f277b8da205e64fac182c58067330
Luiz Henrique <luizhjramos@outlook.com>
2021-08-01 16:06:24 -0300

    Aula 10 - type unknown

---
+++ src/A010-type-unknown/A010-type-unknown.ts
@@ -0,0 +1,10 @@
+
+let x: unknown
+
+x = 10
+
+const y = 800
+
+if (typeof x === 'number') console.log(x + y)
+
+// O tipo unknown só permite que você faça algo com ele se você primeiro fazer a checagem de tipos
