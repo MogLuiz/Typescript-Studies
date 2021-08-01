3a990c88d25818d995b0e2123fc70aa7ececa78e
Luiz Henrique <luizhjramos@outlook.com>
2021-08-01 15:52:10 -0300

    Aula 09 - type enum

---
+++ src/A009-type-enum/A009-type-enum.ts
@@ -0,0 +1,17 @@
+enum Cores {
+  VERMELHO = 10, // 10
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+  ROXO = 'ROXO',
+}
+
+console.log(Cores)
+console.log(Cores.VERMELHO)
+console.log(Cores[10])
+console.log(Cores.ROXO)
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor])
+}
+
+escolhaACor(Cores.ROXO)
