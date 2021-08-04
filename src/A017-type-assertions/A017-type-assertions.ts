f3cf1fcebdb9920f3c4e5ffb145eb4b8ec3a04b7
Luiz Henrique <luizhjramos@outlook.com>
2021-08-03 22:05:03 -0300

    configuração webpack

--- src/A017-type-assertions/A017-type-assertions.ts
+++ src/A017-type-assertions/A017-type-assertions.ts
@@ -1,11 +1,8 @@
+
+/* RECOMENDADO */
 // Condicional (que também faz o refinamento do tipo)
 const body1 = document.querySelector('body')
 if(body1) body1.style.background = 'red'
-
-
-// Non-null assertion (!)
-const body2 = document.querySelector('body')! // Passando a exclamação eu informo que essa constante não é nula
-body2.style.background = 'red'
 
 
 // Type assertion (Ele pega o tipo que foi inferido e fala que essa constante só é do tipo inferido)
@@ -17,5 +14,13 @@ if(body3) body3.style.background = 'red'
 const input = document.querySelector('.input') as HTMLInputElement
 
 
+/* NÃO RECOMENDADO */
+// Non-null assertion (!)
+const body2 = document.querySelector('body')! // Passando a exclamação eu informo que essa constante não é nula
+body2.style.background = 'red'
+
+
 // Subindo tipo na arvore e depois descendo
-const body4 = (document.querySelector('body')
+const body4 = (document.querySelector('body') as unknown) as number //Agora eu consegui passar um tipo number para um elemento HTML subindo e descendo elementos da árvore
+
+
