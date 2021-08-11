33d37c11af4e22d20964f3289434ea8e33f274ee
Luiz Henrique <luizhjramos@outlook.com>
2021-08-10 21:53:33 -0300

    Tipando as constantes que carregam as classes

--- src/A019-exercicio/form-control.ts
+++ src/A019-exercicio/form-control.ts
@@ -1,7 +1,7 @@
 const SHOW_ERROR_MESSAGES = 'show-error-message'
 
-const form = document.querySelector('.form')
-const username = document.querySelector('.username')
-const email = document.querySelector('.email')
-const password = document.querySelector('.password')
-const password2 = document.querySelector('.password2')
+const form = document.querySelector('.form') as HTMLFormElement
+const username = document.querySelector('.username') as HTMLInputElement
+const email = document.querySelector('.email') as HTMLInputElement
+const password = document.querySelector('.password') as HTMLInputElement
+const password2 = document.querySelector('.password2') as HTMLInputElement
