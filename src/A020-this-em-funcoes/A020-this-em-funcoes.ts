ca552bb11b6622b1c6e0610d1721249ec2113859
Luiz Henrique <luizhjramos@outlook.com>
2021-08-11 07:24:15 -0300

    Tipando o this em arrow function

--- src/A020-this-em-funcoes/A020-this-em-funcoes.ts
+++ src/A020-this-em-funcoes/A020-this-em-funcoes.ts
@@ -1,1 +1,11 @@
-import './form-control'
+export function funcao(this: Date, nome: string, age: number): void {
+  console.log(this)
+  console.log(nome, age)
+}
+
+//Tipando o this. O primeiro parâmetro da função não é um argumento e sim a tipagem do this.
+
+funcao.call(new Date(), 'Luiz', 20)
+funcao.apply(new Date(), ['Bruno', 20])
+
+//Chamando uma função com o this tipado.
