9b18044c95da3c7a875c6bd26d02ecc2e2aeda11
Luiz Henrique <luizhjramos@outlook.com>
2021-08-17 07:47:31 -0300

    Aula 46 - Restrições em Generics(constraints)

---
+++ src/Generics/A046-generics-4/A046-generics-4.ts
@@ -0,0 +1,12 @@
+
+type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K]
+
+const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave]
+
+const animal = {
+  cor: 'Rosa',
+  vacinas: ['Vacina1', 'Vacina2']
+}
+
+const vacinas = obterChave(animal, 'cor')
+const vacinas = obterChave(animal, 'vacinas')
