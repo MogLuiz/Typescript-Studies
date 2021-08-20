3fbc96cc3b6d2f010c557c2dd489eb1c072f0fd9
Luiz Henrique <luizhjramos@outlook.com>
2021-08-19 22:23:19 -0300

    A50 Generics - Generics padrão REQUIRED

--- src/Generics/A050-generics-7/A050-generics-7.ts
+++ src/Generics/A050-generics-7/A050-generics-7.ts
@@ -1,6 +1,24 @@
-
+// Record
 const objeto1: Record<string, string | number> = {
   nome: 'Luiz',
   sobrenome: 'Henrique',
   idade: 30,
 }
+
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+}
+
+// Required
+
+type PessoaRequired = Required<PessoaProtocol> // Type Required transforma tudo que é opcional em obrigatório
+
+const objeto2: PessoaRequired = {
+  nome: 'Luiz',
+  sobrenome: 'Henrique',
+  idade: 30,
+}
+
+
